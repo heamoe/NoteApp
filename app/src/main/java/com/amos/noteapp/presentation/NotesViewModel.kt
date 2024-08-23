a26fc04672121db1db10d95a73ba53a638095408
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-08-23 16:41:34 -0400

    add viewModel

--- app/src/main/java/com/amos/noteapp/presentation/NotesViewModel.kt
+++ app/src/main/java/com/amos/noteapp/presentation/NotesViewModel.kt
@@ -1,10 +1,34 @@
 package com.amos.noteapp.presentation
 
+
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.amos.noteapp.data.Note
 import com.amos.noteapp.data.NoteDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class NotesViewModel(
     private val dao: NoteDao
 ) : ViewModel() {
+    private val isSortedByDateAdded = MutableStateFlow(true)
 
+
+    fun onEvent(event: NotesEvent) {
+        when (event) {
+            is NotesEvent.DeleteNote -> {
+                viewModelScope.launch {
+                    dao.deleteNote(event.note)
+                }
+            }
+
+            is NotesEvent.SaveNote -> {
+
+            }
+
+            NotesEvent.SortNotes -> {
+                isSortedByDateAdded.value = !isSortedByDateAdded.value
+            }
+        }
+    }
 }
