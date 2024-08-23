0b0da0e2104a63b9d7bb0473687a23deada4b035
Humanoid Rebel <60774863+heamoe@users.noreply.github.com>
2024-08-23 17:10:32 -0400

    update viewModel

--- app/src/main/java/com/amos/noteapp/presentation/NotesViewModel.kt
+++ app/src/main/java/com/amos/noteapp/presentation/NotesViewModel.kt
@@ -1,18 +1,37 @@
 package com.amos.noteapp.presentation
 
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.amos.noteapp.data.Note
 import com.amos.noteapp.data.NoteDao
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class NotesViewModel(
     private val dao: NoteDao
 ) : ViewModel() {
     private val isSortedByDateAdded = MutableStateFlow(true)
+    private var notes = isSortedByDateAdded.flatMapLatest { sort ->
+        if (sort) {
+            dao.getNotesOrderedByDateAdded()
+        } else {
+            dao.getNotesOrderedByTitle()
+        }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
 
+    val _state = MutableStateFlow(NoteState())
+    val state = combine(_state, isSortedByDateAdded, notes) { state, isSortedByDateAdded, notes ->
+        state.copy(
+            notes = notes
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), NoteState())
 
     fun onEvent(event: NotesEvent) {
         when (event) {
@@ -23,7 +42,21 @@ class NotesViewModel(
             }
 
             is NotesEvent.SaveNote -> {
+                val note = Note(
+                    title = state.value.title.value,
+                    description = state.value.description.value,
+                    dateAdded = System.currentTimeMillis()
+                )
+                viewModelScope.launch {
+                    dao.upsertNote(note)
+                }
 
+                _state.update {
+                    it.copy(
+                        title = mutableStateOf(""),
+                        description = mutableStateOf("")
+                    )
+                }
             }
 
             NotesEvent.SortNotes -> {
